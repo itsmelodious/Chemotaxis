3498c6edb7406865c8e853eb2a3b1b3b00506301
itsmelodious <melodyyan16@gmail.com>
2015-09-17 08:15:31 -0700

    Created pastel colors

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,5 +1,21 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
  Bacteria [] b;   
- void setup()   
+ public void setup()   
  {      
  	size(500, 500);
  	background(160, 207, 195); 
@@ -11,7 +27,7 @@
  		b[i] = new Bacteria(myX, myY);
  	}
  }   
- void draw()   
+ public void draw()   
  {    
  	//move and show the bacteria
  	background(131, 171, 161);
@@ -32,16 +48,25 @@
  		g = (int)(Math.random()*128)+127;
  		b = (int)(Math.random()*128)+127;
  	}
- 	void move()
+ 	public void move()
  	{
  		x = x + (int)(Math.random()*3)-1;
  		y = y + (int)(Math.random()*3)-1;
  	}
- 	void show()
+ 	public void show()
  	{
- 		strokeWeight(0.5);
+ 		strokeWeight(0.5f);
  		stroke(255);
  		fill(r, g, b);
  		ellipse(x, y, 10, 10);
  	}
  }    
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
